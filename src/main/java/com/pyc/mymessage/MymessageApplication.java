0ab9e39fb9d53d320a166870f5602421ed6aa9b7
ycy-ycy <2923616405@qq.com>
2020-02-15 19:50:13 +0800

    Update

--- src/main/java/com/pyc/mymessage/MymessageApplication.java
+++ src/main/java/com/pyc/mymessage/MymessageApplication.java
@@ -1,13 +1,28 @@
 package com.pyc.mymessage;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.integration.dsl.core.Pollers;
+import org.springframework.integration.scheduling.PollerMetadata;
 
 @SpringBootApplication
 public class MymessageApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MymessageApplication.class, args);
+    }
+
+    // using @value annotation to gain resources from https://spring.io/blog.atom by automatic
+    @Value("https://spring.io/blog.atom")
+    Resource resource;
+
+    // using Fluent API and pollers to configure acquiescent way of poll
+    @Bean(name = PollerMetadata.DEFAULT_POLLER)
+    public PollerMetadata poller(){
+        return Pollers.fixedRate(500).get();
     }
 
 }
