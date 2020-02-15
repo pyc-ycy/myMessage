67fd76da1f0d6d06720f9a3809184a2e28fc2cdc
ycy-ycy <2923616405@qq.com>
2020-02-15 19:19:23 +0800

    Initial commit

---
+++ src/main/java/com/pyc/mymessage/MymessageApplication.java
@@ -0,0 +1,13 @@
+package com.pyc.mymessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymessageApplication.class, args);
+    }
+
+}
